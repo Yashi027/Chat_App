a11d044159f2ab2e0839252fe3e24f6977ef96bb
Yashi Bansal <bansalyashi163@gmail.com>
2025-09-27 16:40:14 +0530

    setting up the environment and ui for signing

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import './Login.css'
+import assets from '../../assets/assets.js'
+
+function Login() {
+  return (
+    <div className='login'>
+      <img src={assets.logo_big} className='logo' />
+      <form className="login-form">
+        <h2>Sign Up</h2>
+        <input type="text" placeholder='Username' className="form-input" required />
+        <input type="email" placeholder='Email' className="form-input" required />
+        <input type="password" placeholder='Password' className="form-input" required />
+        <button type='submit'>Sign Up</button>
+        <div className="login-term">
+          <input type="checkbox" />
+          <p>Agree to the terms of use and policy.</p>
+        </div>
+        <div className="login-forgot">
+          <p className="login-toggle">Already have an account?<span>Click here</span></p>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login

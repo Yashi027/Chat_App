7bb679c5a27610dcb8f7ea479c1d5f164e43b3b8
Yashi Bansal <bansalyashi163@gmail.com>
2025-09-28 17:16:07 +0530

    chatbox ui

--- src/components/chatbox/Chatbox.jsx
+++ src/components/chatbox/Chatbox.jsx
@@ -1,10 +1,45 @@
 import React from 'react';
 import './Chatbox.css'
+import assets from '../../assets/assets.js'
 
 const Chatbox = () => {
   return (
-    <div>
-      
+    <div className='chatbox'>
+      <div className="chat-user">
+        <img src={assets.profile_img} alt="" />
+        <p>Richard <img src={assets.green_dot} className='dot' alt="" /></p>
+        <img src={assets.help_icon} alt="Help" className='help' />
+      </div>
+
+      <div className="chat-msg">
+        <div className="s-msg">
+          <p className="msg">
+            Lorem ipsum dolor sit amet consectetur adipisicing
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:32 PM</p>
+          </div>
+        </div>
+        <div className="r-msg">
+          <p className="msg">
+            Lorem ipsum dolor sit amet consectetur adipisicing
+          </p>
+          <div>
+            <img src={assets.profile_img} alt="" />
+            <p>2:34 PM</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="chat-input">
+        <input type="text" placeholder='Send a message' />
+        <input type="file" id='image' accept='image/png image/jpeg' hidden />
+        <label htmlFor="image">
+          <img src={assets.gallery_icon} alt="" />
+        </label>
+        <img src={assets.send_button} alt="" />
+      </div>
     </div>
   );
 }
